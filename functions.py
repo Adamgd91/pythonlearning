3c5ede43934a202b031eab89deb64302754acbcb
Adam Davidson <adamgd91@gmail.com>
2023-02-06 21:12:52 -0500

    lerning

--- functions.py
+++ functions.py
@@ -8,3 +8,13 @@ def counting():
         
 
 
+hours = [30,44,43,50,22,22,30,44,48]
+
+most_hours = []
+
+def practice():
+    for i in hours:
+        if hours.count(i) > 1:
+            most_hours.append(i)
+    print(most_hours)
+practice()  
