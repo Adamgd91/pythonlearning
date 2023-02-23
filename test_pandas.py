b4a7bea87311371dbc84c2dea02ec1f23b9fc5fc
Adam Davidson <agdavidson91@gmail.com>
2023-02-23 08:54:51 -0500

    panda

--- test_pandas.py
+++ test_pandas.py
@@ -6,5 +6,11 @@ print(df)
 
 pd_mean_weight = df["Weight"].mean()
 pd_median_weight = df['Weight'].median()
+pd_position = df[['Position', "Age"]]
+
 print(pd_mean_weight)
 print(pd_median_weight)
+print(pd_position)
+
+row_loc = df.loc[5]
+print(row_loc)
